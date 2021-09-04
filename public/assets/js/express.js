d132e8e368d416ea47448a66706d6a29cabd9de2
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-04 14:26:26 -0700

    express addidtions

---
+++ public/assets/js/express.js
@@ -0,0 +1,15 @@
+//settin gup rroutes
+const express = require('express');
+const apiRoutes = require('public/routes/app-routes.js')
+const htmlRoutes = require('public/routes/html.js')
+
+const app = express();
+const fs = require('fs');
+const path = require('path')
+//Initalizing app 
+var port = PORT(process.env.PORT || '3001')
+app.set('port', port)
+app.listen(3001, () => {
+
+});
+
