6ce49baf1fad9b795f63e27f5383aae2f96c532b
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-04 16:21:07 -0700

    links

--- public/assets/js/express.js
+++ public/assets/js/express.js
@@ -5,11 +5,12 @@ const htmlRoutes = require('public/routes/html.js')
 
 const app = express();
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 //Initalizing app 
-var port = PORT(process.env.PORT || '3001')
-app.set('port', port)
-app.listen(3001, () => {
 
+var port = PORT(process.env.PORT || '3001');
+app.set('port', port);
+app.listen(3001, () => {
+    console.log(`API server on port 3001!`)
 });
 
