fac9bd7a16e3e1d2576bd991a11f02399dc1606a
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-09-05 12:39:28 -0700

    API routes code

---
+++ public/routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const store = require('../db.store');
+
+router.get('.public/notes.html', function(req, res) => {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err=> res.status(500).json(err));
+});
+
+router.post('public/notes.html' , function(req, res) => {
+    store  
+        .addNotes(req.body)
+        .then((notes)=> res.json(notes))
+        .catch(err => res.staus(500).json(err));
+});
+
+//delete 
+
+router.delete('public/notes.html', function(req, res)=>{
+    store
+    .removeNotes(req.params.id)
+    .then(()=> res.json.({ok: true }))
+    .catch(err => res.status(500).json(err));
+});
+module.exports = router;
