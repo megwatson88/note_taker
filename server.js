f4d293f182c7486e6d42df18bb3af8e6a8cc6dcd
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 20:13:31 -0500

    .gitignroe added

---
+++ server.js
@@ -0,0 +1,23 @@
+//settin API rroutes
+const express = require('express');
+const apiRoutes = require('public/routes/app-routes.js')
+const htmlRoutes = require('public/routes/html.js')
+
+// const fs = require('fs');
+// const path = require('path');
+
+//setting up app and port 
+const app = express();
+var port = PORT(process.env.PORT || '3001');
+
+//Initalizing app uses the app.use
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', html)
+//where to set the port 
+app.listen(3001, () => {
+    console.log(`API server on port 3001!`)
+});
+
