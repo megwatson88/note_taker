e7e86dd27e6f887101db97691a9f37efad40a910
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 19:25:21 -0500

    catching up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
 const store = require('../db.store');
+const uuid = require('uuid');
+const path = require('path');
+const fs = require('fs');
+
+
+// how do I set up this path?  
+//let { notes } = require('db/db.json');
+
+const writeDB = () => {
+    fs.writeFileSync(
+        path.join(__dirname, 'db/db.json'),
+        JSON.stringify({notes},null, 2)
+    );
+    return notes;
+}
 
 //adding notes
 router.get('.public/notes.html', function(req, res) {
