c3844ba7437886026dd3dda7d8ca9bff6543cda4
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 22:26:57 -0500

    html routes

--- routes/html.js
+++ routes/html.js
@@ -1,6 +1,9 @@
 let path = require(path);
 let router = require('express').Router();
 
+router.get("/", req,res => {
+    res.sendFile(path.join(__dirname, "../public", "notes.html"))
+});
 
 router.get('.notes', function(req, res){
     res.sendFile(path.join(__dirname, "public/notes.html"));
