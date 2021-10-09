8f6f41bd9e372a2fe1dabf398ec92b608b8c2c92
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-08 20:35:02 -0500

    moving files and fixing server.js

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const apiRoutes = require('public/routes/app-routes.js')
 const htmlRoutes = require('public/routes/html.js')
 
-// const fs = require('fs');
-// const path = require('path');
 
 //setting up app and port 
 const app = express();
-var port = PORT(process.env.PORT || '3001');
+const PORT = PORT(process.env.PORT || '3001');
 
 //Initalizing app uses the app.use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
+
+//setting up routes
 app.use('/api', apiRoutes);
 app.use('/', html)
 //where to set the port 
 app.listen(3001, () => {
-    console.log(`API server on port 3001!`)
+    console.log(`API server on port ${PORT} !`)
 });
 
