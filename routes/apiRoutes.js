f1c2ade1859b9191bf808888c40b8b44ff5e4737
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-10-09 10:55:38 -0500

    make it better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,9 @@ const uuid = require('uuid');
 const path = require('path');
 const fs = require('fs');
 
+//is this how to set up the uuid package? 
+//uuid const { v4 as uuidv4 } = require('uuid')
+//uuidv(); 
 
 // how do I set up this path?  
 //let { notes } = require('db/db.json');
@@ -16,7 +19,7 @@ const writeDB = () => {
     return notes;
 }
 
-//adding notes
+//getting notes
 router.get('/notes', (req, res)  => {
     store
         .getNotes()
@@ -26,7 +29,9 @@ router.get('/notes', (req, res)  => {
 //posting notes 
 
 router.post('/notes' , (req, res) => {
-    store  
+    const newNotes = {
+        id : uuid.v4()
+    } 
         .addNotes(req.body)
         .then((notes)=> res.json(notes))
         .catch(err => res.staus(500).json(err));
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //settin API rroutes
 const express = require('express');
-const apiRoutes = require('public/routes/app-routes.js')
-const htmlRoutes = require('public/routes/html.js')
+const apiRoutes = require('routes/apiRoutes.js');
+const htmlRoutes = require('public/routes/html.js');
 
 
 //setting up app and port 
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 //setting up routes
 app.use('/api', apiRoutes);
-app.use('/', html)
+app.use('/', htmlRoutes);
 //where to set the port 
 app.listen(3001, () => {
     console.log(`API server on port ${PORT} !`)
